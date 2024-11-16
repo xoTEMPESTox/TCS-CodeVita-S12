7f8a2a5be9de357cfa5cdcd988b85c98a48fbf0a
gamer301 <priyanshu123sah@gmail.com>
2024-11-16 13:34:45 +0530

    test with greedy approach

--- R1Z1/B/Justify.py
+++ R1Z1/B/Justify.py
@@ -2,4 +2,26 @@ K = int(input())
 words=[]
 for i in range(K):
     words.append(input())
-N,M=map(int(),input().split())
+N,M=map(int,input().split()) #M Max letter , N no of lines 
+counter=0
+for j in range(N):
+    line=0
+    for i in words:
+
+        word_size=len(i)
+
+        if word_size>M:
+            continue
+
+        if line == 0:
+            line += word_size
+            counter += 1
+        elif word_size+1 + line <=M:
+            line += word_size + 1
+            counter += 1
+    
+    if counter == K:
+        break
+        
+print(counter)   
+
